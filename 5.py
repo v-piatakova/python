baa7ab864a55451c0c128314c07764edfa5fc19f
Violeta <49119241+v-piatakova@users.noreply.github.com>
2021-11-01 21:25:02 +0300

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,10 @@
+
+# string = input("Enter string of numbers")
+def split_by_index(string):
+    print(string)
+    string = str(string)
+    return tuple(string)
+
+    split_by_index(884928678950)
+
+
